2d75fe40190607037a2670791496d48ce4fc40ec
Ethan Snyder <ethan.snyder@usafa.edu>
2013-11-20 21:48:10 -0700

    added basic forward motion to test communication

--- main.c
+++ main.c
@@ -5,35 +5,60 @@
  */
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-    P1DIR|= BIT3; // TA0.0
-    P1DIR|= BIT4; // TA0.1
-    P1DIR|= BIT8; // TA1.0
-    P1DIR|= BIT9; // TA1.1
+    P1DIR|= BIT1; // TA0.0
+    P1DIR|= BIT2; // TA0.1
+    P2DIR|= BIT0; // TA1.0
+    P2DIR|= BIT1; // TA1.1
 
-    	P1DIR |= BIT2|BIT0|BIT1|BIT4;                // TA0CCR1 on P1.BITS
 
-        TACTL &= ~MC1|MC0;         // stop timer A0
+    P1SEL |= BIT1;
+    P1SEL |= BIT2;
+    P2SEL |= BIT0;
+    P2SEL |= BIT1;
 
-        TACTL |= TACLR;                // clear timer A0
+    // set up timers
+    TA0CTL &= ~ MC1|MC0; // stop timer
 
-        TACTL |= TASSEL1;           // configure for SMCLK
+    TA0CTL |= TACLR; // clear timer
 
-        TACCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
+    TA1CTL &= ~ MC1|MC0; // stop timer
 
-        TACCR1 = 25;                // set duty cycle to 25/100 (25%)
+    TA1CTL |= TACLR; // clear timer
 
-        TACCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode   5 for 0
+    TA1CTL |= TASSEL1; // configure SMCLK
 
-        TACTL |= MC0;                // count up
+    //TA0CCTL1 |= OUTMOD_5; //sets to reset
+    TA0CCTL0 |= OUTMOD_7; //sets to reset/reset
 
-        while (1) {
-               __delay_cycles(1000000);
-               TACCR1 = 5;            // set duty cycle to 50/100 (50%)
+    TA1CCTL1 |= OUTMOD_7; //sets to reset
+    //TA1CCTL0 |= OUTMOD_5; //sets to reset
+
+    TA0CCR0 = 100; //sets signal period to 100 clock cycles
+    TA0CCR1 = 50; // sets duty cycle
+
+    TA1CCR0 = 100; //sets signal period to 100 clock cycles
+    TA1CCR1 = 50; // sets duty cycle
+
+    TA0CTL |= MCO; // starts counting up
+    TA1CTL |= MCO;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
            }
-
-
-
 
 
 
